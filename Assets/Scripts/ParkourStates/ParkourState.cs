dcbf91506e8ca57a96baa095dd365d4dac54d79a
Matthew Martin <mmartin724@yahoo.com>
2014-12-29 20:07:54 -0500

    Added parkour state classes

---
+++ Assets/Scripts/ParkourStates/ParkourState.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using Hack.States;
+
+public abstract class ParkourState : State
+{
+
+	public override void Enter()
+	{
+		base.Enter();
+	}
+
+	public override void Update()
+	{
+		base.Update();
+	}
+
+	public override void Exit()
+	{
+		base.Exit();
+	}
+}
---
+++ Assets/Scripts/ParkourStates/VaultingState.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using Hack.States;
+
+public class VaultingState : State
+{
+
+	public override void Enter()
+	{
+		base.Enter();
+	}
+
+	public override void Update()
+	{
+		base.Update();
+	}
+
+	public override void Exit()
+	{
+		base.Exit();
+	}
+}
