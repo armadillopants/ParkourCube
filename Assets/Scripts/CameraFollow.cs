fa4afa4a1d0c3b01cdb218e0a65971df2cc4bfe6
Matthew Martin <mmartin724@yahoo.com>
2015-01-01 15:17:00 -0500

    made changes to camera, simpler

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,24 +4,20 @@ public class CameraFollow : MonoBehaviour
 {
 
 	private Transform target;
-	private Vector3 velocity;
-	private float damp;
+	private float followSpeed;
 
 	void Start()
 	{
 		target = GameObject.FindGameObjectWithTag("Player").transform;
-		velocity = Vector3.zero;
-		damp = 0.1f;
+		followSpeed = 10f;
 	}
 
 	void FixedUpdate()
 	{
 		if (target)
 		{
-			Vector3 point = camera.WorldToViewportPoint(target.position);
-			Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
-			Vector3 destination = transform.position + delta;
-			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, damp);
+			transform.position = Vector2.Lerp(transform.position, target.position, followSpeed * Time.fixedDeltaTime);
+			transform.position -= new Vector3(0, 0, 10);
 		}
 	}
 }
