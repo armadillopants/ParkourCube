3e3cfd9f96eef961a403d5e02e366e2f9436994b
Matthew Martin <mmartin724@yahoo.com>
2015-01-01 15:33:32 -0500

    made it so u can fall out of a slide

--- Assets/Scripts/ParkourStates/SlidingState.cs
+++ Assets/Scripts/ParkourStates/SlidingState.cs
@@ -28,6 +28,13 @@ public class SlidingState : ParkourState
 			if (TryRun()) { return; }
 		}
 
+		RaycastHit2D hit = Physics2D.Linecast(owner.transform.position, owner.transform.position - new Vector3(0, 0.6f, 0), owner.GetLayerMask());
+
+		if (hit.collider == null)
+		{
+			if (TryFall()) { return; }
+		}
+
 		owner.Move(owner.velocity * slideSpeed);
 		LeanTween.rotateZ(owner.gameObject, 45, 5f * Time.fixedDeltaTime);
 	}
