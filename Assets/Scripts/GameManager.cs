8a7d2c55026897cd3e612762447afa12d020e618
Shawn Miller <shawn.miller0@gmail.com>
2015-01-21 13:52:29 -0500

    created GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MSingleton<GameManager>
+{
+	public Canvas menu;
+	public Canvas score;
+	public Canvas postGame;
+
+	void Start()
+	{
+		menu.enabled = true;
+		score.enabled = false;
+		postGame.enabled = false;
+	}
+
+	public void OnPlayButton()
+	{
+		menu.enabled = false;
+		score.enabled = true;
+		World.CreateNewWorld();
+	}
+
+	public void OnQuitButton()
+	{
+		Debug.Break();
+		//Application.Quit();
+	}
+
+	public void OnGameOver()
+	{
+		score.enabled = false;
+		postGame.enabled = true;
+	}
+
+	public void OnGameOverRestart()
+	{
+		score.enabled = true;
+		postGame.enabled = false;
+		World.Clear();
+		World.CreateNewWorld();
+	}
+
+	public void OnGameOverBack()
+	{
+		postGame.enabled = false;
+		menu.enabled = true;
+		World.Clear();
+	}
+}
