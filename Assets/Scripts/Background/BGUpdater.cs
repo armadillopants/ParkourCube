9c4f16083d8653b382a3832954b14ad4b027e158
Shawn Miller <shawn.miller0@gmail.com>
2015-02-04 19:55:32 -0500

    background parallax objects

---
+++ Assets/Scripts/Background/BGUpdater.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class BGUpdater : MonoBehaviour
+{
+	public int initialAmount;
+	public List<GameObject> bgPrefabs;
+	public float zDepth;
+
+	public Transform cam;
+	private Vector3 lastCamPosition;
+	private GameObject container;
+
+	private List<ParallaxObject> bgObjects = new List<ParallaxObject>();
+
+	void Start()
+	{
+		if(container) { DestroyImmediate(container); }
+		bgObjects.Clear();
+
+		container = new GameObject("BG Container");
+		container.transform.position = cam.position;
+		container.transform.parent = cam.transform;
+
+		lastCamPosition = cam.position;
+		PopulateScene();
+	}
+
+	void Update()
+	{
+		Vector3 delta = cam.position - lastCamPosition;
+		delta.x *= -1f;
+		delta.z = 0f;
+
+		foreach(ParallaxObject bgObj in bgObjects)
+		{
+			bgObj.UpdatePosition(delta);
+		}
+
+		lastCamPosition = cam.position;
+	}
+
+	void PopulateScene()
+	{
+		System.Random random = new System.Random();
+		for(int i=0; i<initialAmount; ++i)
+		{
+			Vector3 camPos = new Vector3(Random.value, Random.value, zDepth);
+			Vector3 worldPos = cam.camera.ViewportToWorldPoint(camPos);
+
+			int val = random.Next(0, bgPrefabs.Count);
+			GameObject instance = Instantiate(bgPrefabs[val], worldPos, Quaternion.identity) as GameObject;
+
+			//if(!instance) { continue; }
+			
+			ParallaxObject pObj = instance.GetComponent<ParallaxObject>();
+			pObj.RollRandomLevel();
+
+			instance.transform.parent = container.transform;
+
+			bgObjects.Add(pObj);
+		}
+	}
+}
---
+++ Assets/Scripts/Background/ParallaxObject.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public enum ParallaxLevel
+{
+	Near,
+	Middle,
+	Far
+}
+
+public class ParallaxObject : MonoBehaviour
+{
+	private readonly float[] parallaxValues = { 0.6f, 0.3f, 0.1f };
+	private float parallaxAmount;
+	private bool wasOnScreen;
+
+	public void RollRandomLevel()
+	{
+		System.Random random = new System.Random();
+		parallaxAmount = parallaxValues[random.Next(0, 3)];
+	}
+
+	public void UpdatePosition(Vector3 cDelta)
+	{
+		Vector3 newPos = transform.localPosition;
+		newPos += cDelta * parallaxAmount;
+		transform.localPosition = newPos;
+
+		MoveIfOffScreen();
+	}
+
+	private void MoveIfOffScreen()
+	{
+		if(!renderer.isVisible && wasOnScreen)
+		{
+			wasOnScreen = false;
+			Vector3 newPos = transform.position;
+			newPos.x = Camera.main.ViewportToWorldPoint(Vector2.right).x + collider.bounds.extents.x + 1f;
+			transform.position = newPos;
+		}
+		
+		if(renderer.isVisible)
+		{
+			wasOnScreen = true;
+		}
+	}
+}
